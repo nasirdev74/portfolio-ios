f424f7da6f24d1f3189ab66d8a47f597daa1ee62
nasir <nasir.siraj@softcent.eu>
2022-11-25 18:13:33 +0600

    project created

---
+++ portfolio/ContentView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+       .imageScale(.large)
+       .foregroundColor(.accentColor)
+      Text("Hello, world" )
+    }
+     .padding()
+  }
+}
+
+class ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+
+  #if DEBUG
+  @objc class func injected() {
+    let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+    windowScene?.windows.first?.rootViewController =
+      UIHostingController(rootView: ContentView())
+  }
+  #endif
+}
---
+++ portfolio/portfolioApp.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+@main
+struct portfolioApp: App {
+  init() {
+    #if DEBUG
+    var injectionBundlePath = "/Applications/InjectionIII.app/Contents/Resources"
+    #if targetEnvironment(macCatalyst)
+    injectionBundlePath = "\(injectionBundlePath)/macOSInjection.bundle"
+    #elseif os(iOS)
+    injectionBundlePath = "\(injectionBundlePath)/iOSInjection.bundle"
+    #endif
+    Bundle(path: injectionBundlePath)?.load()
+    #endif
+  }
+
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
